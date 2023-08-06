74451b40c6bf8a8bccd8ebbac993bab8e0c7c6f9
CollinBeurskens <96735577+CollinBeurskens@users.noreply.github.com>
2023-08-06 15:06:13 +0200

    installer.bat

---
+++ installer.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Are you sure you want to install Zen? Y/N
+set /p yn=
+if %yn% == Y goto install
+exit
+
+:install
+echo Installing GIT...
+winget install --id Git.Git -e --source winget>nul
+git clone https://github.com/CollinBeurskens/ZEN.git
+echo Done installing! Exiting...
+timeout 2 /nobreak>nul
+exit
