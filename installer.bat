dc63ca2d36ecaadb6ecc386fd9a7e5ee909cc24f
CollinBeurskens <96735577+CollinBeurskens@users.noreply.github.com>
2023-08-06 15:09:46 +0200

    installer.bat

--- installer.bat
+++ installer.bat
@@ -7,7 +7,7 @@ exit
 :install
 echo Installing GIT...
 winget install --id Git.Git -e --source winget>nul
-git clone https://github.com/CollinBeurskens/ZEN.git
+git clone https://github.com/CollinBeurskens/collinhax.git
 echo Done installing! Exiting...
 timeout 2 /nobreak>nul
 exit
