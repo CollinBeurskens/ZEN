568674ac4e4c44d48dc3b27ee9f1c9087de73c02
CollinBeurskens <96735577+CollinBeurskens@users.noreply.github.com>
2023-08-27 20:45:25 +0200

    Update Py.py

--- Py.py
+++ Py.py
@@ -3,7 +3,7 @@ import time
 
 os.system('clear')
 print("Password for Zen Project V1 : ")
-pw = getpass.getpass(prompt=’Password: ‘, stream=None)
+pw = getpass.getpass(prompt='Password: ', stream=None)
 if pw == ("e!que"):
   print("Correct!")
 else:
